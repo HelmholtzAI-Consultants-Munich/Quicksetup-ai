d45e9fa8f4e6f0333c7055d2b30f3ed394b459a0
gerome.vivar <gerome.vivar@helmholtz-muenchen.de>
2022-05-03 20:11:48 +0200

    feat: MedNIST classification tutorial

---
+++ src/ml_pipeline_template/models/classification.py
@@ -0,0 +1,65 @@
+from typing import Any
+
+import torch
+from pytorch_lightning import LightningModule
+from torchmetrics.classification.accuracy import Accuracy
+
+from ml_pipeline_template import utils
+
+log = utils.get_logger(__name__)
+
+
+class ClassificationTaskModule(LightningModule):
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        lr: float = 0.001,
+        weight_decay: float = 0.0005,
+    ):
+        super().__init__()
+        self.net = net
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
+        self.save_hyperparameters()
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+
+    def step(self, batch: Any):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        return loss, preds, y
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+        acc = self.train_acc(preds, targets)
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+        acc = self.val_acc(preds, targets)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+        acc = self.test_acc(preds, targets)
+        self.log("test/loss", loss, on_step=False, on_epoch=True)
+        self.log("test/acc", acc, on_step=False, on_epoch=True)
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(
+            params=self.parameters(),
+            lr=self.hparams.lr,
+            weight_decay=self.hparams.weight_decay,
+        )
