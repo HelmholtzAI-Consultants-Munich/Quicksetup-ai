b08d42a8605a4935bf3a9520b97284afc829e5ac
gerome.vivar <gerome.vivar@helmholtz-muenchen.de>
2022-05-19 11:30:58 +0200

    fix: print error when installing package

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -2,12 +2,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
+        setup()
+    except Exception as error_instance:  # noqa
+        print(error_instance)
         raise
