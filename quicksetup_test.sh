cafcdffe5ca468263ba5f8744e4a7807b4f0e6d2
Isra Mekki <98828872+IsraMekki0@users.noreply.github.com>
2022-12-21 10:24:53 +0100

    Update quicksetup_test.sh

--- quicksetup_test.sh
+++ quicksetup_test.sh
@@ -1,5 +1,5 @@
 pip install cookiecutter dvc
-yes | cookiecutter https://github.com/HelmholtzAI-Consultants-Munich/Quicksetup-ai.git --replay
+cookiecutter https://github.com/HelmholtzAI-Consultants-Munich/Quicksetup-ai.git --no-input
 cd Quicksetup-ai/
 pip install -e .
 python scripts/train.py trainer.min_epochs=1 trainer.max_epochs=2 log_dir=test_case
