a8080e5b0716fd75348282a6dc362c93dd7789dd
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2022-12-21 13:56:59 +0100

    tests: automated testing

--- quicksetup_test.sh
+++ quicksetup_test.sh
@@ -2,5 +2,6 @@ pip install cookiecutter dvc
 cookiecutter https://github.com/HelmholtzAI-Consultants-Munich/Quicksetup-ai.git --no-input
 cd Quicksetup-ai/
 pip install -e .
-python scripts/train.py trainer.min_epochs=1 trainer.max_epochs=2 log_dir=test_case
-python scripts/test.py ckpt_path=logs/checkpoints/last.ckpt
+# python scripts/train.py trainer.min_epochs=1 trainer.max_epochs=2 log_dir=test_case
+# python scripts/test.py ckpt_path=logs/checkpoints/last.ckpt
+# pytest
